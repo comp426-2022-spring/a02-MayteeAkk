2132299bf0a9e9f8c14c42c152c5f6205a06ae30
MayteeAkk <mayteeoakk@gmail.com>
2022-02-13 00:32:16 -0500

    Trying Again

--- flips.js
+++ flips.js
@@ -5,5 +5,8 @@ import { createRequire } from "module";
 let require = createRequire(import.meta.url);
 let myArgs = minimist(process.argv.slice(2));
 let flips = myArgs['number'] || 1;
+let flipArray = [];
+flipArray = coinFlips(flips);
 
+console.log(flipArray);
 console.log(coinFlips(flips));
